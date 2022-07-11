8ab2721b98880478ee7df5e30cd24b2cb67bda28
Tharukapakaya <109050918+Tharukapakaya@users.noreply.github.com>
2022-07-11 07:41:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94717543085']
+global.premium = ['94717543085']
+global.ownernomer = '94717543085'
+global.ownername = '🐦tharuka'
+global.botname = 'podi shagy'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
